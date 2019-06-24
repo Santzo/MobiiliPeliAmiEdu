e2c72f98505126afdc9df3394b5a6699114d7e96
Santeri Hakoniemi <santeri.hakoniemi@gmail.com>
2019-06-24 19:08:44 +0300

    24.6.2019 Nyt toimii Android buildikin

--- Assets/Scripts/GamePieceManager.cs
+++ Assets/Scripts/GamePieceManager.cs
@@ -21,41 +21,8 @@ public class GamePieceManager : MonoBehaviour
             Destroy(gameObject);
         }
 
-        pieces = LoadAssets<GameObject>("Prefabs/GamePieces");
+        pieces = Resources.LoadAll<GameObject>("GamePieces/");
     }
 
-    private T[] LoadAssets<T>(string path)
-    {
-        List<object> al = new List<object>();
-        string[] fileEntries = Directory.GetFiles(Application.dataPath + "/" + path);
 
-        foreach (string fileName in fileEntries)
-        {
-            if (!fileName.EndsWith(".meta"))
-            {
-                string temp = fileName.Replace("\\", "/");
-                int index = temp.LastIndexOf("/");
-                string localPath = "Assets/" + path;
-
-                if (index > 0)
-                    localPath += temp.Substring(index);
-                Debug.Log(localPath);
-                Object t = AssetDatabase.LoadAssetAtPath(localPath, typeof(T));
-                if (t != null && t.GetType() == typeof(GameObject))
-                {
-                    GameObject a = (GameObject) t;
-                    GamePiece piece = a.GetComponent<GamePiece>();
-
-                    if (piece != null)
-                        al.Add(a);
-                }
-            }
-        }
-
-        T[] result = new T[al.Count];
-        for (int i = 0; i < al.Count; i++)
-            result[i] = (T)al[i];
-
-        return result;
-    }
 }
